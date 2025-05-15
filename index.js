ad42462db527b866459ccd30c6bc39face3f4ac1
JobertDev27 <jobertdev27@gmail.com>
2025-05-15 14:27:25 +0800

    added a url linking to pages

---
+++ index.js
@@ -0,0 +1,22 @@
+import http from "http";
+import fs from "fs";
+import url from "url";
+
+http
+  .createServer((req, res) => {
+    const q = url.parse(req.url, true);
+    const filename = `.${q.pathname}`;
+
+    fs.readFile(filename, (err, data) => {
+      if (err) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("404.html");
+        return res.end();
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      }
+    });
+  })
+  .listen(8080);
